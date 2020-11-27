730a10ba691e6f60a5a6dc0132a1e1313207973e
fbessou <fbessou@free.fr>
2020-11-27 16:24:43 +0000

    Deploying to gh-pages from master @ 05d9fed9f283fc33b44defd1255537576d66c4cb 🚀

--- big-tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="big-tileset" tilewidth="16" tileheight="16" tilecount="21000" columns="140">
- <image source="big-tileset.png" width="2240" height="2400"/>
-</tileset>
